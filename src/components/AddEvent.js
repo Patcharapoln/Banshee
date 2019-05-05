752ded90f30d61dc7740909881bc34eea44d89b6
Patcharapoln <patcharapol.n@ku.th>
2019-05-05 08:03:21 +0700

    :heavy_plus_sign: Create add event page

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -1,8 +1,100 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView
+} from 'react-native'
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars'
 
-const AddEvent = () => {
-  return <View><Text>Test</Text></View>
+class AddEvent extends React.Component {
+  state = {
+    date: '',
+    event: '',
+    value: ''
+  }
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Calendar
+            onDayPress={day => {
+              this.setState({ date: day.dateString })
+              console.log(this.state.date)
+            }}
+            markedDates={{
+              [this.state.date]: {
+                selected: true,
+                selectedColor: 'rgb(81,181,249)'
+              }
+            }}
+          />
+          <TextInput
+            style={styles.textInput}
+            autoCapitalize="none"
+            placeholder="Event"
+            // onChangeText={str => this.setState({ email: str })}
+          />
+          <TextInput
+            style={styles.textInput}
+            autoCapitalize="none"
+            placeholder="Value"
+            // onChangeText={str => this.setState({ email: str })}
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.IncomeButton}>
+              <Text style={{ color: 'white', fontSize:18 }}>Add Income</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.ExpenseButton}>
+              <Text style={{ color: 'white', fontSize:18 }}>Add Expense</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  IncomeButton: {
+    width: '30%',
+    borderRadius: 5,
+    alignItems: 'center',
+    marginRight: 10,
+    padding: 10,
+    backgroundColor: 'rgb(128,221,172)'
+  },
+
+  ExpenseButton: {
+    width: '30%',
+    borderRadius: 5,
+    alignItems: 'center',    
+    marginLeft: 10,
+    padding: 10,
+    backgroundColor: 'rgb(240,128,128)'
+  },
+  textInput: {
+    borderBottomWidth: 0.25,
+    borderColor: 'rgb(128,128,128)',
+    fontSize: 18,
+    marginTop: 10,
+    marginBottom: 20,
+    height: 35,
+    width: 275
+  }
+})
 
 export default AddEvent
--- src/components/Ledger.js
+++ src/components/Ledger.js
@@ -1,8 +1,58 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {
+  StyleSheet,
+  View,
+  Image,
+  Dimensions,
+  ScrollView,
+  Text
+} from 'react-native'
 
-const Ledger = () => {
-  return <View><Text>Test</Text></View>
+var width = Dimensions.get('window').width //full width
+var height = Dimensions.get('window').height
+
+class Ledger extends React.Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center'
+        }}
+      >
+        <Image source={require('../../assets/money.jpg')} style={styles.logo} />
+        <ScrollView style={{backgroundColor:'#1D2951',width:'100%'}}>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+          <Text>sadsadasd</Text>
+        </ScrollView>
+
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: width,
+    height: width * 0.33911,
+    resizeMode: 'contain',
+    alignItems: 'center'
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#1D2951',
+    marginTop: 20
+  }
+})
 
 export default Ledger
